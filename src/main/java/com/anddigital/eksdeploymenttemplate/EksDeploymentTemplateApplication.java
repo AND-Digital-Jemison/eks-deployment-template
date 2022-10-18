6483e87711941f5edae0b6028d23b6e029eff14e
Jack <jackrimmer2996@hotmail.co.uk>
2022-10-18 10:26:38 +0100

    Reinitialised with Springboot

---
+++ src/main/java/com/anddigital/eksdeploymenttemplate/EksDeploymentTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.anddigital.eksdeploymenttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksDeploymentTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EksDeploymentTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anddigital/eksdeploymenttemplate/EksDeploymentTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anddigital.eksdeploymenttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksDeploymentTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
